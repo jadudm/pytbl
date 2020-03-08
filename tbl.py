527b16bdecbf73b874103922cf3038a1f2c1e1c7
Matt Jadud <matt@jadud.com>
2020-03-08 15:03:10 -0400

    Adding in work from 20200308
    I'm going to take this one step at a time. This will be a good exercise
    for me, and keep me from going to fast. I want to get tests, structure,
    and other things in place from the beginning, but I'll do it in a way
    that illustrates concepts for future use.
    
    In other words, the repository will become a teaching tool.

---
+++ tbl.py
@@ -0,0 +1,19 @@
+#The MIT License
+# Copyright (c) Matthew C. Jadud <matt@jadud.com>
+
+# This code is being developed in conjunction with a series of blog
+# posts at https://jadud.com/. Many aspects are therefore being 
+# staged, so that they are incrementally addressed, as opposed to
+# being implemented "all-at-once".
+
+# Imports
+import csv
+import requests
+
+def tbl_from_sheet(csv_url):
+  with requests.Session() as s:
+    download = s.get(csv_url)
+    content = download.content.decode('utf-8')
+    reader = csv.reader(content.splitlines(), delimiter=',')
+    for row in list(reader):
+      print (row)
