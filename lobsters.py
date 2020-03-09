ed5dc258799a31de885f07993f3a341861577424
Matt Jadud <matt@jadud.com>
2020-03-08 20:45:53 -0400

    Transitioning to OO.
    This looks like it will be a class. I'll later come around and consider
    a functional wrapper around the class. Perhaps I should do it the other
    way, but... this is a good exploration, regardless.

--- lobsters.py
+++ lobsters.py
@@ -1,4 +1,6 @@
 import tbl
 
 pets_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSK2rd47ogfI2CpQi2L6HDo9AOEhnhqBN4zR4kLPUO28vBzmlc8XQWrvTfBYCU0ePf478yxcNKdOy5m/pub?gid=0&single=true&output=csv"
-pets_tbl = tbl.from_sheet(pets_url)
+
+a_tbl = tbl.tbl(url = pets_url)
+a_tbl.show_columns()
--- tbl/__init__.py
+++ tbl/__init__.py
@@ -9,11 +9,47 @@
 # Imports
 import csv
 import requests
+from types import SimpleNamespace
+from collections import namedtuple as NT
 
-def from_sheet(csv_url):
-  with requests.Session() as s:
-    download = s.get(csv_url)
-    content = download.content.decode('utf-8')
-    reader = csv.reader(content.splitlines(), delimiter=',')
-    for row in list(reader):
-      print (row)
+# NamedTuples provide simple record-type structures for giving
+# names to information. I don't need full classes here (yet),
+# but I don't want to use plain lists or tuples, because down that road
+# lies madness. There will ultimately be quite a few, and I'll probably
+# move them into their own namespace.
+Column = NT("Column", ["name", "type"])
+
+class tbl:
+
+  def __init__(self, url=None, has_header=True):
+    # A SimpleNamespace is really an empty object that
+    # lets me set, modify, and delete attributes.
+    # http://bit.ly/3aHq2Rx
+    self.fields = SimpleNamespace()
+
+    # Initialize fields
+    self.fields.columns = list()
+
+    # Handle keywords
+    if url:
+      self.from_sheet(url, has_header=has_header)
+
+  def _add_column(self, column):
+    self.fields.columns.append(column)
+
+  def from_sheet(self, url, has_header=True):
+    self.fields.url = url
+    with requests.Session() as s:
+      download = s.get(self.fields.url)
+      content = download.content.decode('utf-8')
+      reader = csv.reader(content.splitlines(), delimiter=',')
+      # FIXME: I need to conditionally handle the absence of a header.
+      if has_header:
+        header_row = next(reader)
+        for name in header_row:
+          # FIXME: I need to guess the type of the column here.
+          self._add_column(Column(name=name, type=None))
+
+  def show_columns (self):
+    for c in self.fields.columns:
+      print(c.name)
